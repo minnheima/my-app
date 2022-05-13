9f38df5b24e741cbe362886ebd8ee9464d8795dc
minnheima <hertz.minn@gmail.com>
2022-05-14 00:08:12 +0900

    (new project)주소록만들기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+// import { info } from 'node-sass';
+import React, { Component } from 'react';
 import './App.css';
+import PhoneForm from './Components/PhoneForm';
+import PhoneInfoList from './Components/PhoneInfoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+class App extends Component {
+  //id는 렌더링 될 필요가 없기 때문에 state 넣어주지 않아도 된다
+  id = 0;
+  state = {
+    information: [],
+  }
+
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat(Object.assign({}, data, {
+        id: this.id++ 
+      }))
+    });
+  }
+  handleRemove = (id)=> {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    });
+  }
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => {
+          if(info.id === id){
+            return {
+              id,
+              ...data,
+            };
+          }
+          return info;
+        }
+      )
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
+        <PhoneInfoList 
+          data={this.state.information} 
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
