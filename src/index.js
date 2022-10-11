915f71b559591c520de5182e08900b3ed2263e40
minnheima <hertz.minn@gmail.com>
2022-10-11 20:50:00 +0900

    add action creator

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// the functions only return object (addTodo, deleteTodo)
+const addTodo = (text) => {
+  return { type: ADD_TODO, text };
+};
+const deleteTodo = (id) => {
+  return { type: DELETE_TODO, id };
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
@@ -25,6 +33,14 @@ const reducer = (state = [], action) => {
 const store = createStore(reducer);
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAdd = (text) => {
+  store.dispatch(addTodo(text));
+};
+const dispatchDel = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+};
+
 const paintTodos = () => {
   const toDos = store.getState();
   ul.innerHTML = ""; // ul안의 li값을 없애줌
@@ -32,7 +48,7 @@ const paintTodos = () => {
     const li = document.createElement("li");
     const btn = document.createElement("button");
     btn.innerText = "Del";
-    btn.addEventListener("click", deleteTodo);
+    btn.addEventListener("click", dispatchDel);
     li.id = toDo.id;
     li.innerText = toDo.text;
     li.appendChild(btn);
@@ -41,20 +57,11 @@ const paintTodos = () => {
 };
 store.subscribe(paintTodos); //store가 바뀔때마다 repainting해서 계속 이전 값들도 보임 so! -> line29 ul.innerHTML = "";
 
-const addTodo = (text) => {
-  store.dispatch({ type: ADD_TODO, text });
-};
-
-const deleteTodo = (e) => {
-  // console.log(e.target.parentNode.id);
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch({ type: DELETE_TODO, id });
-};
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addTodo(toDo);
+  dispatchAdd(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
