52cd01426385142fef798f8711e2f4ca5a7fb42d
minnheima <hertz.minn@gmail.com>
2022-10-11 15:13:18 +0900

    improvement

--- src/index.js
+++ src/index.js
@@ -8,14 +8,21 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 number.innerText = 0;
+
+//2nd imporvemnet )) 변수 상수를 이용해서 string값을 관리하면 오류를 줄일 수 있다
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 //action은 redux에서 function을 부를 때 쓰는 두번째 parameter or argument다
+//1st improvement )) if else 보다 switch를 사용하는 것이 좋다
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -28,10 +35,10 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 add.addEventListener("click", handleAdd);
 minus.addEventListener("click", handleMinus);
