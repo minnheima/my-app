dca7759e3a6261752b2aa19d60c49c5e6896d1b0
minnheima <hertz.minn@gmail.com>
2022-10-11 15:00:32 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,42 +1,117 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 // Hooks 공부
+// ---------------------------- useTabs -------------------------------
+// const content = [
+//   {
+//     tab: "Section 1",
+//     cont: "I'm the content of the Section 1",
+//   },
+//   {
+//     tab: "Section 2",
+//     cont: "I'm the content of the Section 2",
+//   },
+// ];
 
-const content = [
-  {
-    tab: "Section 1",
-    cont: "I'm the content of the Section 1",
-  },
-  {
-    tab: "Section 2",
-    cont: "I'm the content of the Section 2",
-  },
-];
+// const useTabs = (iniTab, allTabs) => {
+//   const [currentIdx, setCurrentIdx] = useState(iniTab);
+//   if (!allTabs || !Array.isArray(allTabs)) {
+//     return;
+//   }
+//   return {
+//     currentItem: allTabs[currentIdx],
+//     changeItem: setCurrentIdx,
+//   };
+// };
 
-const useTabs = (iniTab, allTabs) => {
-  const [currentIdx, setCurrentIdx] = useState(iniTab);
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-  return {
-    currentItem: allTabs[currentIdx],
-    changeItem: setCurrentIdx,
-  };
-};
+// const App = () => {
+//   const { currentItem, changeItem } = useTabs(1, content);
+
+//   return (
+//     <div className="App">
+//       {content.map((section, idx) => (
+//         <button onClick={() => changeItem(idx)} key={section.tab}>
+//           {section.tab}
+//         </button>
+//       ))}
+//       <div className="content-box">{currentItem.cont}</div>
+//     </div>
+//   );
+// };
+// export default App;
+
+// ------------------------- useEffect ------------------------------
+
+// const App = () => {
+//   const [number, setNumber] = useState(0);
+//   const [aNumber, setAnumber] = useState(0);
+//   const sayHello = () => console.log("hello!");
+//   useEffect(() => {
+//     sayHello();
+//   }, []); //[]만 넣으면 sayHellor가 한번만 실행된다
+//   return (
+//     <div className="App">
+//       <div>Hi</div>
+//       <button onClick={() => setNumber(number + 1)}>{number}</button>
+//       <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+//     </div>
+//   );
+// };
+// export default App;
+
+// -------------------------- useTitle ---------------------------
+// 문서 제목을 업데이트 해주는
+// const useTitle = (initialTitle) => {
+//   const [title, setTitle] = useState(initialTitle);
+//   const updateTitle = () => {
+//     const htmlTitle = document.querySelector("title");
+//     htmlTitle.innerText = title;
+//   };
+//   useEffect(updateTitle, [title]);
+//   return setTitle;
+// };
+// const App = () => {
+//   const titleUpdater = useTitle("Loading...");
+//   setTimeout(() => titleUpdater("Home"), 5000);
+//   return (
+//     <div className="App">
+//       <div>Hi</div>
+//     </div>
+//   );
+// };
+// export default App;
+
+// -------------------------- useClick ---------------------------
+
+// const useClick = (onClick) => {
+//   const element = useRef();
+//   useEffect(() => {
+//     if (element.current) {
+//       element.current.addEventListener("click", onClick); //component mount 되었을 때 event를 추가해주고
+//     }
+//     return () => {
+//       if (element.current) {
+//         element.current.removeEventListener("click", onClick);
+//       }
+//     }; //useEffect는 componentWillUnmount 때
+//   }, []);
+//   if (typeof onClick !== "function") {
+//     return;
+//   }
+//   return element;
+// };
 
 const App = () => {
-  const { currentItem, changeItem } = useTabs(1, content);
-
-  return (
-    <div className="App">
-      {content.map((section, idx) => (
-        <button onClick={() => changeItem(idx)} key={section.tab}>
-          {section.tab}
-        </button>
-      ))}
-      <div className="content-box">{currentItem.cont}</div>
-    </div>
-  );
+  // const sayHello = () => console.log("say hello");
+  // const title = useClick(sayHello);
+  // return (
+  //   <div className="App">
+  //     <h1 ref={title}>Hi</h1>
+  //   </div>
+  // );
+  return null;
 };
 export default App;
+
+///-------------------  연습중
--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App";
+import "./index.css";
+import { legacy_createStore as createStore } from "redux";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+number.innerText = 0;
+//action은 redux에서 function을 부를 때 쓰는 두번째 parameter or argument다
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
+};
 
+const countStore = createStore(countModifier);
 
-// ========================================
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+//count value update
+countStore.subscribe(onChange);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
